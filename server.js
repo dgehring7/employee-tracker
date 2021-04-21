b15b475deb3e090ee2bfbadf583395ea68ba6da0
Dgehring <dgehring7@gmail.com>
2021-04-20 23:52:27 -0400

    let user see options in seeOpt function and set 2 queries in role and dept

--- server.js
+++ server.js
@@ -39,8 +39,48 @@ const connection = mysql.createConnection({
       });
     }
 
-// 
+// Lets user see options and navigate through departments, roles, and employee roles
+    const seeOpt = () => {
+      inquirer
+       .prompt({
+         name: 'route',
+         type: 'list',
+         message: "What would you like to see?",
+         choices: ['See Department', 'See Role', 'See Employee', 'See Employees by Manager', 'Return']
+       })
+       .then((answer) => {
+         switch(answer.route){
+           case 'See Department':
+             return seeDept();
+            case 'See Role':
+             return seeRole();
+            case 'See Employee':
+             return seeEmpl();
+            case 'See Employee by Manager':
+             return seeEmpl('byMan');
+            case 'Return':
+             return starter();
+         }
+       });
+    }
 
+// Lets the user see all the departments
+    const seeDept = () => {
+      connection.query('SELECT department.name as Department, CONCAT("$", SUM(role.salary, 0)) as Budget, Count(employee.id) as Employee Count FROM department LEFT JOIN role on role.department_id = department.id LEFT JOIN employee ON employee.role_id = role.id GROUP BY department;', (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        starter();
+      })
+    }
+
+// Lets user see all the roles
+    const seeRole = () => {
+      connection.query('SELECT role.title as Role, Concat("$", role.salary) as Salary, department.name AS Department FROM role Join department ON role.department_id;', (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        starter();
+      })
+    }
 
 
 
